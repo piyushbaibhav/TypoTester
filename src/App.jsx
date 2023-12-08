24c71119911964b2380910e1d1921b856da3bf09
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-09 02:03:42 +0530

    word cloud display

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './App.css'
+
+const getCloud = () => `coding piyush react nextjs mongodb frontend backend cloud devops faang placement javascript gate happy wood world blanket house car`.split(' ').sort(()=> Math.random()>0.5?1:-1)
 
 function App() {
 
   const[userInput, setUserInput]=useState('')
+  const cloud = useRef(getCloud())
   
 
   return (
     <div>
       <h1>typing test</h1>
 
-      <p>{userInput} </p>
+      <p>{cloud.current.join(' ')} </p>
 
       <input 
         type="text" 
