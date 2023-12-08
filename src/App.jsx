9cbdcea62c5b29287052d60120ade9d9f28d5695
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-09 02:23:59 +0530

    move to next word after space

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useRef, useState } from 'react'
 
 import './App.css'
 
-const getCloud = () => `coding piyush react nextjs mongodb frontend backend cloud devops faang placement javascript gate happy wood world blanket house car`.split(' ').sort(()=> Math.random()>0.5?1:-1)
+const getCloud = () => `coding piyush react nextjs mongodb frontend backend cloud devops faang placement javascript gate happy wood world blanket house car`.split(' ')
+// .sort(()=> Math.random()>0.5?1:-1)
 
 function App() {
 
   const[userInput, setUserInput]=useState('')
   const cloud = useRef(getCloud())
   const [activeWordIndex, setactiveWordIndex]=useState(0)
+
+  function processInput(value){
+    if (value.endsWith(' ')){
+      setactiveWordIndex(index=>index+1)
+      setUserInput('')
+    }else{
+      setUserInput(value)
+    }
+  }
   
 
   return (
@@ -29,7 +39,8 @@ function App() {
 
       <input 
         type="text" 
-        value={userInput} onChange={(e) => setUserInput(e.target.value)
+        value={userInput} 
+        onChange={(e) => processInput(e.target.value)
       } />
     </div>
   )
