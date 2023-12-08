a35a3bfd991083491775f0582b73428e8ecb87e5
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-09 03:15:38 +0530

    optimizing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import './App.css'
 const getCloud = () => `coding piyush react nextjs mongodb frontend backend cloud devops faang placement javascript gate happy wood world blanket house car`.split(' ')
 // .sort(()=> Math.random()>0.5?1:-1)
 
+function Word(props){
+
+  const {text, active}= props
+  return <span style={{
+    fontWeight: active?'bold':'normal'
+  }} >{text} </span>
+}
+
 function App() {
 
   const[userInput, setUserInput]=useState('')
@@ -26,12 +34,10 @@ function App() {
       <h1>typing test</h1>
 
       <p>{cloud.current.map((word, index)=>{
-        if(index===activeWordIndex){
-          return <b>{word} </b>
-        }
-        // return <span key={index}>{word}</span>
-        return <span>{word} </span> 
-          
+        return <Word 
+                text = {word}
+                active={index===activeWordIndex}
+                />
       
       
       
