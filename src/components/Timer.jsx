9d316bda70564019c131fd31d77fc136fcd18489
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-09 23:08:46 +0530

    WPM

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -16,7 +16,7 @@ function Timer(props) {
   const minutes= timeElapsed/60
   return <div>
              <p>Time: {timeElapsed} </p>
-             <p>Speed: {(correctWords/minutes) || 0} WPM </p>
+             <p>Speed: {((correctWords/minutes) || 0).toFixed(2)} WPM </p>
          </div>
 }
 
