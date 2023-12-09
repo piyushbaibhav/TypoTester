2e319a82b630dc13f806af411a7c83a9e282f416
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-10 05:11:00 +0530

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 function Footer() {
   return (
     <div className="flex justify-center mt-8">
-      <a className="mr-4" href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+      <a className="mr-4" href="https://github.com/piyushbaibhav" target="_blank" rel="noopener noreferrer">
         GitHub
       </a>
-      <a className="mr-4" href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+      <a className="mr-4" href="https://www.linkedin.com/in/piyush-baibhav-02098b272/" target="_blank" rel="noopener noreferrer">
         LinkedIn
       </a>
-      <a href="https://github.com/your-repository/fork" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/piyushbaibhav/TypoTester" target="_blank" rel="noopener noreferrer">
         Fork
       </a>
     </div>
