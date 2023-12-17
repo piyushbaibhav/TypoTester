18f391876ede79cc93a09a3e9c39521c908c6665
Marwan Mohammed <53322831+JAGMWPT123@users.noreply.github.com>
2023-12-17 14:07:02 +0200

    Update socketModule.cjs

--- src/api/socketModule.cjs
+++ src/api/socketModule.cjs
@@ -3,7 +3,7 @@
 import { io } from 'socket.io-client';
 
 
-const socket = io('http://localhost:5000');
+const socket = io('https://typo-server.vercel.app/');
 
 // Event listeners and other socket-related code
 export const initSocket = () => {
