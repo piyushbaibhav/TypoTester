716aa157101245ca66aef2a94ba3fce6c6a9f8ef
Marwan Mohammed <53322831+JAGMWPT123@users.noreply.github.com>
2023-12-17 23:11:39 +0200

    Update socketModule.cjs

--- src/api/socketModule.cjs
+++ src/api/socketModule.cjs
@@ -7,25 +7,24 @@ const socket = io("https://typo-server.vercel.app");
 
 // Event listeners and other socket-related code
 export const initSocket = () => {
-  return  if (socket) {
-
-  // Listen for typing challenge state updates from the server
-  socket.on("challengeState", (challengeState) => {
-    setStartCounting(challengeState.startCounting);
-    setactiveWordIndex(challengeState.activeWordIndex);
-    setCorrectWordArray(challengeState.correctWordArray);
-    setRemainingTime(challengeState.remainingTime);
-    setTotalWordsAttempted(challengeState.totalWordsAttempted);
-  });
-  // Listen for user join event
-  socket.on("userJoin", (joinedUsername) => {
-    setRoomUsers((users) => [...users, joinedUsername]);
-  });
-  // Listen for user disconnect event
-  socket.on("userDisconnect", (disconnectedUsername) => {
-    console.log(`${disconnectedUsername} has disconnected.`);
-    // Handle user disconnection, e.g., remove the user from the UI
-  });
+   if (socket) {
+      // Listen for typing challenge state updates from the server
+      socket.on("challengeState", (challengeState) => {
+        setStartCounting(challengeState.startCounting);
+        setactiveWordIndex(challengeState.activeWordIndex);
+        setCorrectWordArray(challengeState.correctWordArray);
+        setRemainingTime(challengeState.remainingTime);
+        setTotalWordsAttempted(challengeState.totalWordsAttempted);
+      });
+      // Listen for user join event
+      socket.on("userJoin", (joinedUsername) => {
+        setRoomUsers((users) => [...users, joinedUsername]);
+      });
+      // Listen for user disconnect event
+      socket.on("userDisconnect", (disconnectedUsername) => {
+        console.log(`${disconnectedUsername} has disconnected.`);
+        // Handle user disconnection, e.g., remove the user from the UI
+      });
     return socket;
   }
 };
