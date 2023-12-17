4720fbc51680012581c01dc538402a42ed202193
Marwan Mohammed <53322831+JAGMWPT123@users.noreply.github.com>
2023-12-17 15:53:44 +0200

    Update socketModule.cjs

--- src/api/socketModule.cjs
+++ src/api/socketModule.cjs
@@ -3,7 +3,7 @@
 import { io } from 'socket.io-client';
 
 
-const socket = io("https://typo-server.vercel.app/");
+const socket = io("https://typo-server.vercel.app/"), { transports: ['websocket'] });
 //   (server, {
 //     cors: {
 //       origin: '*',
